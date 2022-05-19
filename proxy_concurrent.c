462bec7ea034d7b650c3b15f060e08763a603696
Ubuntu <ubuntu@ip-172-31-47-154.ap-northeast-2.compute.internal>
2022-05-19 00:53:50 +0000

    chore: 설명을 위한 주석 작성

--- proxy_concurrent.c
+++ proxy_concurrent.c
@@ -7,14 +7,16 @@
 
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
-    "Firefox/10.0.3\r\n";
+    "Firefox/10.0.3\r\n"; /*user의 os정보등을 제공하여 서버가 이에 맞게 다른 return 값을 줄 수도 있다 */
 static const char *requestline_hdr_format = "GET %s HTTP/1.0\r\n";
 static const char *eof = "\r\n";
-static const char *host_hdr_format = "Host: %s\r\n";
-static const char *conn_hdr = "Connection: close\r\n";
-static const char *prox_hdr = "Proxy-Connection: close\r\n";
+static const char *host_hdr_format = "Host: %s\r\n"; /* Host 헤더를 작성하는 것은 
+                                                        가상 호스팅을 할 경우 (하나의 서버(같은IP)를 사용하는 여러 어플리케이션들 사이에어서 
+                                                        목적지를 알 수 있다 */
+static const char *conn_hdr = "Connection: close\r\n"; /* 문제 조건 */
+static const char *prox_hdr = "Proxy-Connection: close\r\n"; /* 문제 조건 */
 
-static const char *host_key = "Host";
+static const char *host_key = "Host"; 
 static const char *connection_key = "Connection";
 static const char *proxy_connection_key = "Proxy-Connection";
 static const char *user_agent_key = "User-Agent";
@@ -37,24 +39,24 @@ int main(int argc, char **argv)
   struct sockaddr_storage clientaddr;
   pthread_t tid; /* pthread의 자료형 */
 
-  if (argc != 2)
+  if (argc != 2) /* 인자로 포트 번호를 받는다 */
   {
     fprintf(stderr, "usage: %s <port> \n", argv[0]);
     exit(1); 
   }
 
-  listenfd = Open_listenfd(argv[1]); /*듣기 소켓을 생성하고 listenfd를 받는다 */
+  listenfd = Open_listenfd(argv[1]); /*듣기 소켓을 생성하고(g) listenfd를 받는다(getaddrinfo, socket(), bind() */
   while (1)
   {
     clientlen = sizeof(clientaddr);
     connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); /* 연결 되면 connfd 반환 */
 
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); 
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); /* 클라이언트 정보를 hostname과 port로 나눠 해당 변수에 저장 */
     printf("Accepted connection from (%s %s).\n", hostname, port);
 
-    /* 호출된 함수에서 쓰레드를 생성한다 */
-    /* thread 식별자, 쓰레드 특성 지정 사용시 부여하는 값(기본NULL), 분기시켜서 실행할 함수, 실행할 함수에 매개변수로 넘겨지는 값*/
-    /* doit(connfd), Close(connfd); 를 thread 함수 내에서 (다른 쓰레드에서) 실행할 것 */
+    /* 호출된 함수에서 쓰레드를 생성한다 
+       thread 식별자, 쓰레드 특성 지정 사용시 부여하는 값(기본NULL), 분기시켜서 실행할 함수, 실행할 함수에 매개변수로 넘겨지는 값
+       doit(connfd), Close(connfd); 를 thread 함수 내에서 (다른 쓰레드에서) 실행할 것 */
     /* connection 맺어질 떄마다 새로운 쓰레드에서 작업한다 */
     Pthread_create(&tid, NULL, thread, (void *)connfd); 
   }
@@ -81,7 +83,7 @@ void doit(int connfd)
   rio_t client_rio, server_rio; 
   int end_serverfd; /*the end server file descriptor*/
 
-  Rio_readinitb(&client_rio, connfd);
+  Rio_readinitb(&client_rio, connfd); /*connfd를 client_rio랑 연결하고 읽어 buf에 저장 */
   Rio_readlineb(&client_rio, buf, MAXLINE);
   /*read the client request line*/
   sscanf(buf, "%s %s %s", method, uri, version); 
@@ -158,7 +160,7 @@ void build_req_msg(char *req_msg, char *hostname, char *path, int port, rio_t *c
     if (!strcmp(buf, eof))
       break;
 
-    if (!strncasecmp(buf, host_key, strlen(host_key)))
+    if (!strncasecmp(buf, host_key, strlen(host_key))) /* host 헤더 있을 경우 host_hdr에 넣어준다 */
     {
       strcpy(host_hdr, buf);
       continue;
@@ -176,7 +178,7 @@ void build_req_msg(char *req_msg, char *hostname, char *path, int port, rio_t *c
     sprintf(host_hdr, host_hdr_format, hostname);
   }
 
-  sprintf(req_msg, "%s%s%s%s%s%s%s",
+  sprintf(req_msg, "%s%s%s%s%s%s%s", /* 고정값들은 미리 지정한 string으로 넣어주고, other_hdr에 넣은 다른 헤더들도 넣어준다 */
           request_hdr,
           host_hdr,
           conn_hdr,
@@ -190,32 +192,32 @@ void build_req_msg(char *req_msg, char *hostname, char *path, int port, rio_t *c
 inline int connect_endServer(char *hostname, int port)
 {
   char port_str[100];
-  sprintf(port_str, "%d", port);
-  return Open_clientfd(hostname, port_str);
+  sprintf(port_str, "%d", port); /*port를 string으로 넣어준다 */
+  return Open_clientfd(hostname, port_str); /* 이제는 소켓이 클라이언트 입장에서 서버랑 연결을 시도한다 */
 }
 
 void parse_uri(char *uri, char *hostname, char *path, int *port, char *query)
 {
   *port = 80; /*포트번호 입력되지 않으면 기본 80 */
 
-  char *hostname_start_p = strstr(uri, "//");
+  char *hostname_start_p = strstr(uri, "//"); /* "https://의 //부분 구분자" */
   hostname_start_p = hostname_start_p + 2;
 
-  char *query_start_p = strstr(hostname_start_p, "?");
+  char *query_start_p = strstr(hostname_start_p, "?"); /* url뒤에 붙는 query string */
   if (query_start_p != NULL)
   {
     *query_start_p = '\0';
     sscanf(query_start_p + 1, "%s", query);
   }
 
-  char *path_start_p = strstr(hostname_start_p, "/");
+  char *path_start_p = strstr(hostname_start_p, "/"); /* path의 시작 부분 */
   if (path_start_p != NULL)
   {
     sscanf(path_start_p, "%s", path);
     *path_start_p = '\0';
   }
 
-  char *port_start_p = strstr(hostname_start_p, ":");
+  char *port_start_p = strstr(hostname_start_p, ":"); /* 포트 번호 */
   if (port_start_p != NULL)
   {
     *port_start_p = '\0';
--- proxy_sequential.c
+++ proxy_sequential.c
@@ -3,12 +3,14 @@
 
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
-    "Firefox/10.0.3\r\n";
+    "Firefox/10.0.3\r\n"; /*user의 os정보등을 제공하여 서버가 이에 맞게 다른 return 값을 줄 수도 있다 */
 static const char *requestline_hdr_format = "GET %s HTTP/1.0\r\n";
 static const char *eof = "\r\n";
-static const char *host_hdr_format = "Host: %s\r\n";
-static const char *conn_hdr = "Connection: close\r\n";
-static const char *prox_hdr = "Proxy-Connection: close\r\n";
+static const char *host_hdr_format = "Host: %s\r\n"; /* Host 헤더를 작성하는 것은 
+                                                        가상 호스팅을 할 경우 (하나의 서버(같은IP)를 사용하는 여러 어플리케이션들 사이에어서 
+                                                        목적지를 알 수 있다 */
+static const char *conn_hdr = "Connection: close\r\n"; /* 문제 조건 */
+static const char *prox_hdr = "Proxy-Connection: close\r\n"; /* 문제 조건 */
 
 static const char *host_key = "Host";
 static const char *connection_key = "Connection";
@@ -30,19 +32,19 @@ int main(int argc, char **argv)
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
 
-  if (argc != 2)
+  if (argc != 2) /* 인자로 포트 번호를 받는다 */
   {
     fprintf(stderr, "usage: %s <port> \n", argv[0]);
     exit(1); 
   }
 
-  listenfd = Open_listenfd(argv[1]); /*듣기 소켓을 생성하고 listenfd를 받는다 */
+  listenfd = Open_listenfd(argv[1]); /*듣기 소켓을 생성하고 listenfd를 받는다(getaddrinfo, socket(), bind()) */
   while (1)
   {
     clientlen = sizeof(clientaddr);
     connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); /* 연결 되면 connfd 반환 */
 
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); 
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); /* 클라이언트 정보를 hostname과 port로 나눠 해당 변수에 저장 */
     printf("Accepted connection from (%s %s).\n", hostname, port);
 
     doit(connfd);
@@ -62,10 +64,10 @@ void doit(int connfd)
   int end_serverfd; /*the end server file descriptor*/
   int port;
 
-  Rio_readinitb(&client_rio, connfd);
+  Rio_readinitb(&client_rio, connfd); /*connfd를 client_rio랑 연결하고 읽어 buf에 저장 */
   Rio_readlineb(&client_rio, buf, MAXLINE);
   /*read the client request line*/
-  sscanf(buf, "%s %s %s", method, uri, version); 
+  sscanf(buf, "%s %s %s", method, uri, version); /* tiny.c와 달리(path만 온다) uri에 http://~를 포함하는 전체 주소가 오게 된다 */
 
   if (strcasecmp(method, "GET")) /* GET 메소드 아니면 에러 */
   {
@@ -139,7 +141,7 @@ void build_req_msg(char *req_msg, char *hostname, char *path, int port, rio_t *c
     if (!strcmp(buf, eof))
       break;
 
-    if (!strncasecmp(buf, host_key, strlen(host_key)))
+    if (!strncasecmp(buf, host_key, strlen(host_key))) /* host 헤더 있을 경우 host_hdr에 넣어준다 */
     {
       strcpy(host_hdr, buf);
       continue;
@@ -152,12 +154,12 @@ void build_req_msg(char *req_msg, char *hostname, char *path, int port, rio_t *c
     }
   }
 
-  if (strlen(host_hdr) == 0)
+  if (strlen(host_hdr) == 0) /*host header가 없었다면 넣어준다 */
   {
     sprintf(host_hdr, host_hdr_format, hostname);
   }
 
-  sprintf(req_msg, "%s%s%s%s%s%s%s",
+  sprintf(req_msg, "%s%s%s%s%s%s%s", /* 고정값들은 미리 지정한 string으로 넣어주고, other_hdr에 넣은 다른 헤더들도 넣어준다 */
           request_hdr,
           host_hdr,
           conn_hdr,
@@ -171,32 +173,32 @@ void build_req_msg(char *req_msg, char *hostname, char *path, int port, rio_t *c
 inline int connect_endServer(char *hostname, int port)
 {
   char port_str[100];
-  sprintf(port_str, "%d", port);
-  return Open_clientfd(hostname, port_str);
+  sprintf(port_str, "%d", port); /*port를 string으로 넣어준다 */
+  return Open_clientfd(hostname, port_str); /* 이제는 소켓이 클라이언트 입장에서 서버랑 연결을 시도한다 */
 }
 
 void parse_uri(char *uri, char *hostname, char *path, int *port, char *query)
 {
   *port = 80; /*포트번호 입력되지 않으면 기본 80 */
 
-  char *hostname_start_p = strstr(uri, "//");
+  char *hostname_start_p = strstr(uri, "//"); /* "https://의 //부분 구분자" */
   hostname_start_p = hostname_start_p + 2;
 
-  char *query_start_p = strstr(hostname_start_p, "?");
+  char *query_start_p = strstr(hostname_start_p, "?"); /* url뒤에 붙는 query string */
   if (query_start_p != NULL)
   {
     *query_start_p = '\0';
     sscanf(query_start_p + 1, "%s", query);
   }
 
-  char *path_start_p = strstr(hostname_start_p, "/");
+  char *path_start_p = strstr(hostname_start_p, "/"); /* path의 시작 부분 */
   if (path_start_p != NULL)
   {
     sscanf(path_start_p, "%s", path);
     *path_start_p = '\0';
   }
 
-  char *port_start_p = strstr(hostname_start_p, ":");
+  char *port_start_p = strstr(hostname_start_p, ":"); /* 포트 번호 */
   if (port_start_p != NULL)
   {
     *port_start_p = '\0';
